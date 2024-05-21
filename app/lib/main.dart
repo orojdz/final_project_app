94c9672f0ee9e6291b2d979baff6decdbee64654
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-21 20:53:30 +0200

    main

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,72 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:app/screen/inicio.dart';
 
+// Punto de entrada de la aplicación
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Lets eat!',
       theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 246, 108, 69)),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: InicioScreen(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _name = '';
-
-  void grettings() {
-    setState(() {
-      _name = 'Oriana';
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Welcome',
-            ),
-            Text(
-              '$_name',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: grettings,
-        tooltip: 'Add',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
