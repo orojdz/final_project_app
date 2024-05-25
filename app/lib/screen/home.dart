91033215c2a85c2aeb868adcc210086af3def678
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-25 23:47:35 +0200

    WIP

--- app/lib/screen/home.dart
+++ app/lib/screen/home.dart
@@ -6,48 +6,44 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  TextEditingController _searchController = TextEditingController();
-  String _searchText = "";
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(14.0),
-        child: Row(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top + 24,
+          bottom: MediaQuery.of(context).padding.bottom + 24,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              'Categorias',
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Text(
+                'What are you cooking today?',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 233, 130, 50)),
+              ),
             ),
+            SizedBox(height: 12.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: 'Search recipes',
+                    suffixIcon: Icon(Icons.search),
+                    contentPadding: EdgeInsets.all(20),
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(30.0))),
+              ),
+            )
           ],
         ),
       ),
-      //child: TextField(
-      //  controller: _searchController,
-      //  decoration: InputDecoration(
-      //    hintText: 'Search',
-      //    suffixIcon: Icon(Icons.search),
-      //    border: OutlineInputBorder(
-      //      borderRadius: BorderRadius.circular(25.0),
-      //      borderSide: BorderSide.none,
-      //    ),
-      //    contentPadding: EdgeInsets.all(10.0),
-      //    filled: true,
-      //    fillColor: Colors.white,
-      //  ),
-      //  onChanged: (text) {
-      //    setState(() {
-      //      _searchText = text;
-      //    });
-      //  },
-      //),
+      backgroundColor: Color.fromARGB(231, 250, 248, 238),
     );
-  }
-
-  @override
-  void dispose() {
-    _searchController.dispose();
-    super.dispose();
   }
 }
