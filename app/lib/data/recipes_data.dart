d0ee9c7abde9d930cdfc839305881ae68308d95a
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-26 16:02:07 +0200

    wip - model recipe

--- app/lib/data/recipes_data.dart
+++ app/lib/data/recipes_data.dart
@@ -1,1 +1,30 @@
-// apiKey Edamam api 3f9b9cafdb517ba96a67adacf488f3f9
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:app/model/recipe.dart';
+
+// Clase para gestionar las peticiones a la API Edamam
+class EdamamApi {
+  final String appId;
+  final String appKey;
+  final String baseUrl;
+
+  EdamamApi(
+      {required this.appId,
+      required this.appKey,
+      this.baseUrl = 'https://api.edamam.com'});
+
+  Future<List<Recipe>> getRecipesByCategory(String category) async {
+    final url =
+        Uri.parse('$baseUrl/search?q=$category&app_id=$appId&app_key=$appKey');
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      final List<dynamic> hits = data['hits'];
+      return hits.map((hit) => Recipe.fromJson(hit['recipe'])).toList();
+    } else {
+      throw Exception('Failed to load recipes');
+    }
+  }
+}
