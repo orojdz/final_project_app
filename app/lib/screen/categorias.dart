9b274d3ffe9153ed74274b3a1ca6e62e1bffd424
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-26 16:01:49 +0200

    extraída la classe Categorias de home

---
+++ app/lib/screen/categorias.dart
@@ -0,0 +1,67 @@
+import 'package:app/model/categories.dart';
+import 'package:flutter/material.dart';
+
+/* CATEGORIAS */
+class Categorias extends StatelessWidget {
+  const Categorias({
+    super.key,
+    required this.categories,
+    required this.selectCategory,
+  });
+
+  final List<Categories> categories;
+  final void Function(Categories) selectCategory;
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Container(
+      height: 120,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.separated(
+            separatorBuilder: (context, index) => SizedBox(width: 20),
+            // llista de categories
+            itemCount: categories.length,
+            scrollDirection: Axis.horizontal,
+            padding: EdgeInsets.only(left: 20, right: 20),
+            itemBuilder: ((context, index) {
+              return InkWell(
+                // widget clicable (detecta el clic)
+                onTap: () => selectCategory(
+                    categories[index]), // selecciona la categoria
+                child: Container(
+                  width: screenWidth * 0.2,
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 230, 180, 63),
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.white, shape: BoxShape.circle),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.asset(
+                            categories[index].img,
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
+                      Text(categories[index].name,
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 14,
+                              color: Colors.black)),
+                    ],
+                  ),
+                ),
+              );
+            })),
+      ),
+    );
+  }
+}
