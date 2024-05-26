7013e254ca817a67f6d89a2c203935bb8d8269a8
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-26 16:02:53 +0200

    recipes model

--- app/lib/model/recipe.dart
+++ app/lib/model/recipe.dart
@@ -1,1 +1,16 @@
+// Modelo de clase Recipe
+class Recipe {
+  final String label;
+  final String image;
+  final String url;
 
+  Recipe({required this.label, required this.image, required this.url});
+
+  factory Recipe.fromJson(Map<String, dynamic> json) {
+    return Recipe(
+      label: json['label'],
+      image: json['image'],
+      url: json['url'],
+    );
+  }
+}
