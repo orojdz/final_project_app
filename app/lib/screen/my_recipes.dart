7a911063f4980c24bf249d6d4d30eeae1e453308
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-27 11:51:57 +0200

    work in process: My recipes page

--- app/lib/screen/my_recipes.dart
+++ app/lib/screen/my_recipes.dart
@@ -14,10 +14,40 @@ class MyRecipes extends StatelessWidget {
       floatingActionButton: Padding(
         padding: const EdgeInsets.all(8.0),
         child: IconButton(
-          onPressed: () {},
+          onPressed: () {
+            _displayFormSheet(context);
+          },
           icon: Icon(Icons.add),
           color: Color.fromARGB(255, 232, 7, 7),
           tooltip: 'New recipe',
+        ),
+      ),
+    );
+  }
+
+  Future _displayFormSheet(BuildContext context) {
+    return showModalBottomSheet(
+      context: context,
+      builder: (context) => Container(
+        height: 600,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              ListTile(
+                title: Text(
+                  'Create a new recipe',
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: Color.fromARGB(255, 208, 87, 12)),
+                ),
+                trailing: Icon(Icons.close),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
