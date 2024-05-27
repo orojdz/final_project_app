73e77607e2d45912eba6eb486ac18929ed871d71
cf20orianarojas <rojasoriana260217@gmail.com>
2024-05-27 02:35:28 +0200

    main

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -15,8 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      //home: InicioScreen(), descomentar despues
-      home: HomeScreen(),
+      home: InicioScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
